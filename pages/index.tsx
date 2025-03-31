630828d9092395afd92960c628e5dc49a37c83e2
EwertonSA <ewerton.60@hotmail.com>
2025-03-31 16:57:05 -0300

    Criação do header da página homeNoAuth estilo e responsividade

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,19 @@
 import Head from 'next/head';
+import styles from '../styles/HomeNoAuth.module.scss'
+import HeaderNoAuth from '@/src/components/homeNoAuth/headerNoAuth';
 
 const HomeNoAuth= ()=>{
   return(
     <>
     <Head>
-<title>Home</title>
+<title>Comandas</title>
+<link rel="shortcut icon" href="/favicon.jpg" type="image/x-icon" />
+<meta property='og:title' content='Comandas' key='title'/>
+<meta name='description' content='Tenha acesso ao serviço de comandas facilitador para restaurantes'/>
     </Head>
-    <main></main>
+    <main>
+      <HeaderNoAuth/>
+    </main>
     </>
   )
 }
---
+++ src/components/homeNoAuth/headerNoAuth/index.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import styles from './styles.module.scss';
+import {Container,Button} from "reactstrap"
+ 
+const HeaderNoAuth=function (){
+    return <>
+    <div className={styles.ctaSection}>
+<img src="/favicon.jpg" alt="logoCta" className={styles.imgCta} />
+<p>Acesse o melhor aplicativo para restaurantes</p>
+<img src="/favicon.jpg" alt="logoCta" className={styles.imgCta} />
+    </div>
+    <Container className={styles.nav}>
+<img src="/2.jpg" alt="logo" className={styles.imgNav}/>
+<div>
+   <Link href=''>
+   <Button className={styles.navBtn} outline >Pesquisar</Button>
+  
+   </Link>
+   <Link href=''>
+   <Button outline  className={styles.navBtn}>Cadastre</Button>
+   </Link>
+
+</div>
+    </Container>
+    </>
+}
+export default HeaderNoAuth
