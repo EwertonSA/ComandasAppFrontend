5b00d5eaad27bf3292157372d914c1a5b23ef9f1
EwertonSA <ewerton.60@hotmail.com>
2025-06-10 18:12:46 -0300

    Adicionando página de apresentação do portifólio

--- src/component/render/cards/static/indexCertifications.tsx
+++ src/component/render/cards/static/indexCertifications.tsx
@@ -2,16 +2,16 @@ import styles from "../../../../../styles/getStyles.module.scss"
 const IndexCertifications=()=>{
 return(
     <main >
-<p className={styles.title}><img src={"/logoCta.svg"}></img>Certificações</p>
-<p className={styles.subtitle}><img src={"/logoCta.svg"}></img>HTML5</p>
-<p className={styles.subtitle}><img src={"/logoCta.svg"}></img>CSS</p>
-<p className={styles.subtitle}><img src={"/logoCta.svg"}></img>Bootstrap e Sass</p>
-<p className={styles.subtitle}><img src={"/logoCta.svg"}></img>React</p>
-<p className={styles.subtitle}><img src={"/logoCta.svg"}></img>Next</p>
-<p className={styles.subtitle}><img src={"/logoCta.svg"}></img>Node</p>
-<p className={styles.subtitle}><img src={"/logoCta.svg"}></img>Banco de dados SQL</p>
-<p className={styles.subtitle}><img src={"/logoCta.svg"}></img>SQL no Node e Prisma ORM</p>
-<p className={styles.subtitle}><img src={"/logoCta.svg"}></img>MongoDB</p>
+<p className={styles.title}><img src={"/logoCta.png"}></img>Certificações</p>
+<p className={styles.subtitle}><img src={"/logoCta.png"}></img>HTML5</p>
+<p className={styles.subtitle}><img src={"/logoCta.png"}></img>CSS</p>
+<p className={styles.subtitle}><img src={"/logoCta.png"}></img>Bootstrap e Sass</p>
+<p className={styles.subtitle}><img src={"/logoCta.png"}></img>React</p>
+<p className={styles.subtitle}><img src={"/logoCta.png"}></img>Next</p>
+<p className={styles.subtitle}><img src={"/logoCta.png"}></img>Node</p>
+<p className={styles.subtitle}><img src={"/logoCta.png"}></img>Banco de dados SQL</p>
+<p className={styles.subtitle}><img src={"/logoCta.png"}></img>SQL no Node e Prisma ORM</p>
+<p className={styles.subtitle}><img src={"/logoCta.png"}></img>MongoDB</p>
 
 <p></p>
     </main>
