8ef3d02021acafd237b950212a4f631bc9f1ecc6
EwertonSA <ewerton.60@hotmail.com>
2025-06-16 19:21:03 -0300

    Adicionando certificações

--- src/component/render/cards/static/indexCertifications.tsx
+++ src/component/render/cards/static/indexCertifications.tsx
@@ -6,16 +6,24 @@ return(
         <div className=" pt-5">
 <p className={styles.title}>Certificações</p>
 <div className={styles.certifications}> 
-<Link href={"/certificate.pdf"} target="blank"><p className={styles.tech}><img src={"/logoCta.png"} className={styles.Img}></img>HTML5</p></Link>
-<Link href={"/css.pdf"} target="blank"><p className={styles.tech}><img src={"/logoCta.png"}className={styles.Img}></img>CSS</p></Link>
-<Link href={"/certificate.pdf"} target="blank"><p className={styles.tech}><img src={"/logoCta.png"}className={styles.Img}></img>Bootstrap e Sass</p></Link>
-<Link href={"/certificate.pdf"} target="blank"><p className={styles.tech}><img src={"/logoCta.png"}className={styles.Img}></img>React</p></Link>
+<Link href={"/certificatdo_HTML5.pdf"} target="blank"><p className={styles.tech}><img src={"/logoCta.png"} className={styles.Img}></img>HTML5</p></Link>
+<Link href={"/certificado_CSS3.pdf"} target="blank"><p className={styles.tech}><img src={"/logoCta.png"}className={styles.Img}></img>CSS</p></Link>
+<Link href={"/certificado_BootstrapeSass.pdf"} target="blank"><p className={styles.tech}><img src={"/logoCta.png"}className={styles.Img}></img>Bootstrap e Sass</p></Link>
+<Link href={"/certificado_JavascriptI.pdf"} target="blank"><p className={styles.tech}><img src={"/logoCta.png"}className={styles.Img}></img>Javascript I</p></Link>
+<Link href={"/certificado_JavascriptII.pdf"} target="blank"><p className={styles.tech}><img src={"/logoCta.png"}className={styles.Img}></img>Javascript II</p></Link>
+<Link href={"/certificado_JavascriptIII.pdf"} target="blank"><p className={styles.tech}><img src={"/logoCta.png"}className={styles.Img}></img>Javascript III</p></Link>
+<Link href={"/certificado_JavascriptIV.pdf"} target="blank"><p className={styles.tech}><img src={"/logoCta.png"}className={styles.Img}></img>Javascript IV</p></Link>
+<Link href={"/certificado_JavascriptV.pdf"} target="blank"><p className={styles.tech}><img src={"/logoCta.png"}className={styles.Img}></img>Javascript V</p></Link>
+<Link href={"/certificado_JavascriptVI.pdf"} target="blank"><p className={styles.tech}><img src={"/logoCta.png"}className={styles.Img}></img>Javascript VI</p></Link>
+<Link href={"/certificado_JavascriptVI.pdf"} target="blank"><p className={styles.tech}><img src={"/logoCta.png"}className={styles.Img}></img>Typescript</p></Link>
+<Link href={"/certificado_GiteGithub.pdf"} target="blank"><p className={styles.tech}><img src={"/logoCta.png"}className={styles.Img}></img>Git&GitHub</p></Link>
+<Link href={"/certificado_FundamentosdoReact.pdf"} target="blank"><p className={styles.tech}><img src={"/logoCta.png"}className={styles.Img}></img>React</p></Link>
 <Link href={"/certificate.pdf"} target="blank"><p className={styles.tech}><img src={"/logoCta.png"}className={styles.Img}></img>Next</p></Link>
-<Link href={"/certificate.pdf"} target="blank"><p className={styles.tech}><img src={"/logoCta.png"}className={styles.Img}></img>Node</p></Link>
+<Link href={"/certificado_Node.js.pdf"} target="blank"><p className={styles.tech}><img src={"/logoCta.png"}className={styles.Img}></img>Node</p></Link>
 <Link href={"/certificate.pdf"} target="blank"><p className={styles.tech}><img src={"/logoCta.png"}className={styles.Img}></img>Banco de dados SQL</p></Link>
 <Link href={"/certificate.pdf"} target="blank"><p className={styles.tech}><img src={"/logoCta.png"}className={styles.Img}></img>SQL no Node e Prisma ORM</p></Link>
-<Link href={"/certificate.pdf"} target="blank"><p className={styles.tech}><img src={"/logoCta.png"}className={styles.Img}></img>MongoDB</p></Link>
-<Link href={"/certificate.pdf"} target="blank"><p className={styles.tech}><img src={"/logoCta.png"}className={styles.Img}></img>PHP MySQL</p></Link>
+<Link href={"/certificado_PhpMysql.pdf"} target="blank"><p className={styles.tech}><img src={"/logoCta.png"}className={styles.Img}></img>PhpMySQL</p></Link>
+
 </div>
 <p></p>
 </div>
