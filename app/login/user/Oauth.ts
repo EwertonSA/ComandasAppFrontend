85febf91c7d5b6c87ff870625d72e5f816fb5fb0
EwertonSA <ewerton.60@hotmail.com>
2025-08-18 17:21:35 -0300

    Encerrando o app corretamente com logout

--- app/login/user/Oauth.ts
+++ app/login/user/Oauth.ts
@@ -1,7 +1,7 @@
 import crypro from 'crypto'
 export const FACEBOOK_AUTH_URL = `https://www.facebook.com/v21.0/dialog/oauth?` +
   `client_id=${process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID}` +
-  `&redirect_uri=${encodeURIComponent("https://esadev.com.br/api/auth/linkedin/callback")}` +
+  `&redirect_uri=https://esadev.com.br/api/auth/instagram/callback` +
   `&scope=email,public_profile` +
   `&response_type=code`;
  
