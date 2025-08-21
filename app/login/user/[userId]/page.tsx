962245530bbf3a8f3ad2efd847e3978ebdc95422
EwertonSA <ewerton.60@hotmail.com>
2025-08-21 18:01:49 -0300

    Teste para 2fa

--- app/login/user/[userId]/page.tsx
+++ app/login/user/[userId]/page.tsx
@@ -1,8 +1,8 @@
 'use client';
 import { useEffect, useState } from "react";
 import Image from "next/image";
-import authService from "@/src/services/authService";
 import { Verify2FAAction } from "../verify2fa";
+import authService from "@/src/services/authService";
 
 
 export default function Google2FA({ params }: any) {
@@ -10,47 +10,44 @@ export default function Google2FA({ params }: any) {
 
   const [qrCode, setQrCode] = useState<string | null>(null);
   const [token, setToken] = useState("");
-  const [showTokenInput, setShowTokenInput] = useState(false);
 
   useEffect(() => {
     async function fetchQR() {
-      try {
-        const res = await authService.setup2faService(userId);
-        setQrCode(res.qrCodeDataURL);
-        setShowTokenInput(true);
-      } catch (err) {
-        console.error(err);
-      }
+      if (!userId) return;
+      const res = await authService.setup2faService(userId);
+      setQrCode(res.qrCodeDataURL); // usa o qrCode retornado do serviço
     }
     fetchQR();
   }, [userId]);
 
   async function handleVerify(e: React.FormEvent) {
     e.preventDefault();
+    if (!userId) return;
     await Verify2FAAction(Number(userId), token);
   }
 
   return (
     <form onSubmit={handleVerify}>
       <h1>2FA para usuário {userId}</h1>
+
       {qrCode && (
         <div>
           <p>Escaneie o QR Code no Authenticator:</p>
           <Image src={qrCode} alt="QR Code 2FA" width={200} height={200}/>
         </div>
       )}
-      {showTokenInput && (
-        <div>
-          <label>Código 2FA</label>
-          <input
-            name="token"
-            value={token}
-            onChange={(e) => setToken(e.target.value)}
-            maxLength={6}
-            required
-          />
-        </div>
-      )}
+
+      <div>
+        <label>Código 2FA</label>
+        <input
+          name="token"
+          value={token}
+          onChange={(e) => setToken(e.target.value)}
+          maxLength={6}
+          required
+        />
+      </div>
+
       <button type="submit">Confirmar</button>
     </form>
   );
