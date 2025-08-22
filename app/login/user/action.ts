aafc15652d901d9a63d4e148185c86a11ec97a93
EwertonSA <ewerton.60@hotmail.com>
2025-08-22 11:19:10 -0300

    Adicionando reCAPTCHA ao login

--- app/login/user/action.ts
+++ app/login/user/action.ts
@@ -7,8 +7,8 @@ import authService from "@/src/services/authService";
 export async function LoginAction(formData: FormData) {
   const email = formData.get("email")?.toString() || "";
   const password = formData.get("password")?.toString() || "";
-
-  const res = await authService.login({ email, password });
+const recaptchaToken = formData.get("recaptchaToken")?.toString() || "";
+  const res = await authService.login({ email, password,recaptchaToken });
 
   if (res.status === 200) {
     const setcookie=await cookies()
