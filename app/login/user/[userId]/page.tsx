4537332998b290c65dcabe6a4a3669f9ce32a0f9
EwertonSA <ewerton.60@hotmail.com>
2025-09-01 14:35:49 -0300

    Adicionando oAuth com linkedin

--- app/login/user/[userId]/page.tsx
+++ app/login/user/[userId]/page.tsx
@@ -57,11 +57,11 @@ export default function Google2FA({ params }: any) {
       {qrCode && (
         <div>
           <p className={styles.subtitle}>Escaneie este QR Code no seu Authenticator:</p>
-          <Image src={qrCode} alt="QR Code 2FA" width={300} height={300}/>
+          <Image src={qrCode} alt="QR Code 2FA" width={300} height={300} className={styles.main}/>
         </div>
       )}
       <Form onSubmit={handleVerify} >
-        <div className="d-flex flex-column">
+        <div className={styles.main}>
         <label>Código 2FA</label>
         <input
           type="text"
