8d0230fc213dae18b4b79782e755b147a3a6e709
EwertonSA <ewerton.60@hotmail.com>
2025-09-01 17:14:01 -0300

    Criando metadata global no Rootlayout do employeeApp

--- app/employeeApp/layout.tsx
+++ app/employeeApp/layout.tsx
@@ -3,48 +3,6 @@ import HeaderAuth from "@/src/components/common/headerAuth"
 import { Metadata } from "next";
 import { ReactNode } from "react"
 
-export const metadata: Metadata = {
-  title: {
-    default: "EmployeeApp - Restaurante",
-    template: "%s | EmployeeApp",
-  },
-  description:
-    "Aplicativo interno para colaboradores de restaurantes realizarem pedidos de clientes com rapidez e eficiência.",
-  keywords: [
-    "restaurante",
-    "pedidos",
-    "app de pedidos",
-    "colaboradores",
-    "garçom digital",
-    "atendimento",
-  ],
-  openGraph: {
-    title: "EmployeeApp - Restaurante",
-    description:
-      "Facilite o atendimento do seu restaurante: app para colaboradores registrarem pedidos de clientes.",
-    url: "https://esadev.com.br/indexComandas",
-    siteName: "EmployeeApp",
-    images: [
-      {
-        url: "https://esadev.com.br/og-image.jpg",
-        width: 1200,
-        height: 630,
-        alt: "EmployeeApp - Restaurante",
-      },
-    ],
-    locale: "pt_BR",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "EmployeeApp - Restaurante",
-    description:
-      "App para colaboradores de restaurantes registrarem pedidos de clientes.",
-    images: ["https://esadev.com.br/og-image.jpg"],
-  },
-  metadataBase: new URL("https://esadev.com.br"),
-};
-
 const RootLayoutEmployeeApp = ({children}:{children:ReactNode}) => {
     return (
         <div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,54 @@ import AosInitializer from '@/src/components/common/hooks/produtos/aosInit';
 import '../styles/globals.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { ReactNode } from "react"
+import { Metadata } from 'next';
 
+
+export const metadata: Metadata = {
+  title: {
+    default: "EmployeeApp - Restaurante",
+    template: "%s | EmployeeApp",
+  },
+  description:
+    "Aplicativo interno para colaboradores de restaurantes realizarem pedidos de clientes com rapidez e eficiência.",
+  keywords: [
+    "restaurante",
+    "pedidos",
+    "app de pedidos",
+    "colaboradores",
+    "garçom digital",
+    "atendimento",
+  ],
+  openGraph: {
+    title: "EmployeeApp - Restaurante",
+    description:
+      "Facilite o atendimento do seu restaurante: app para colaboradores registrarem pedidos de clientes.",
+    url: "https://esadev.com.br/indexComandas",
+    siteName: "EmployeeApp",
+    images: [
+      {
+        url: "https://esadev.com.br/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "EmployeeApp - Restaurante",
+      },
+    ],
+    locale: "pt_BR",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "EmployeeApp - Restaurante",
+    description:
+      "App para colaboradores de restaurantes registrarem pedidos de clientes.",
+    images: ["https://esadev.com.br/og-image.jpg"],
+  },
+  metadataBase: new URL("https://esadev.com.br"),
+};
 
 export default function Layout({children}:{children:ReactNode}){
+
+  
 return(
  <html lang="pt-BR">
   <body>
