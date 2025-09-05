f740dafe6af3b9a625c3d144c3f9200c7ebd1501
EwertonSA <ewerton.60@hotmail.com>
2025-09-05 17:01:45 -0300

    Adicionando state para proteger a url ao fazer login

--- src/components/common/headerGeneric/index.tsx
+++ src/components/common/headerGeneric/index.tsx
@@ -76,10 +76,10 @@ const comandaId=params.comandaId as string
               <Link href={`/homeNoAuth/${comandaId}`} legacyBehavior>
                 <a className={styles.modalLink}>Página inicial</a>
               </Link>
-              <Link href={`/homeNoAuth/orderCard/${comandaId}`} legacyBehavior>
+              <Link href={`/homeNoAuth/${comandaId}/orderCard`} legacyBehavior>
                 <a className={styles.modalLink}>Carrinho</a>
               </Link>
-              <Link href={`/homeNoAuth/payment/${comandaId}`} legacyBehavior>
+              <Link href={`/homeNoAuth/${comandaId}/payment`} legacyBehavior>
                 <a className={styles.modalLink}>Pagamento</a>
               </Link>
               <a
