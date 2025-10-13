ca125e6cf9a19059266a53d34253ac6ebc9e3608
EwertonSA <ewerton.60@hotmail.com>
2025-10-13 16:44:44 -0300

    Adicionando login do adminjs no frontend

--- app/login/user/action.ts
+++ app/login/user/action.ts
@@ -1,18 +1,15 @@
 "use server";
 
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
 import authService from "@/src/services/authService";
 
 export async function LoginAction(formData: FormData) {
   const email = formData.get("email")?.toString() || "";
   const password = formData.get("password")?.toString() || "";
-  {/*const recaptchaToken = formData.get("recaptchaToken")?.toString() || "";*/}
   const role = formData.get("role")?.toString() as "admin" | "user" | "cliente";
 
   const res = await authService.login({ email, password, role });
 
-  // 🔹 Se o backend pedir 2FA, devolve isso pro frontend
+  // Backend retornou necessidade de 2FA
   if (res.data.twoFARequired) {
     return {
       twoFARequired: true,
@@ -21,10 +18,9 @@ export async function LoginAction(formData: FormData) {
     };
   }
 
-  // 🔹 Se o login for normal (sem 2FA)
-  if (res.status === 200 && res.data.token) {
-    const setcookie = await cookies();
-  setcookie.set("comandas-token", res.data.token, { httpOnly: true });
- 
-  }
+  // Login sem 2FA → backend já retornou token via cookie
+  return {
+    twoFARequired: false,
+    user: res.data.user || null,
+  };
 }
--- app/login/user/verify2fa.tsx
+++ app/login/user/verify2fa.tsx
@@ -1,27 +1,22 @@
 "use server";
 
-import { cookies } from "next/headers";
-import authService from "@/src/services/authService";
 import { redirect } from "next/navigation";
+import authService from "@/src/services/authService";
 
 export async function Verify2FAAction(userId: number, token: string) {
   const res = await authService.verify2fa({ userId, token });
 
-  if (res?.status !== 200) {
-    throw new Error("Código inválido");
+  if (res!.status !== 200) {
+    throw new Error("Código 2FA inválido");
   }
 
+  // ✅ Cookie HttpOnly já setado pelo backend → frontend não precisa manipular
+  const userRole = res!.data.user.role;
 
-  // ✅ Agora o cookie já foi setado pelo navegador.
-  const cookieToken = (await cookies()).get("comandas-token");
-  console.log("Token no cookie:", cookieToken?.value);
-   const userRole = res.data.user.role;
-  
-    if (userRole === "admin") redirect("/admin");
-    if (userRole === "user") redirect("/employeeApp");
-    if (userRole === "cliente") redirect("/clientApp");
-  
-  // 🔹 Falha no login
-  redirect("/login/index");
+  if (userRole === "admin") return redirect("/admin");
+  if (userRole === "user") return redirect("/employeeApp");
+  if (userRole === "cliente") return redirect("/clientApp");
 
+  // fallback
+  return redirect("/login/index");
 }
